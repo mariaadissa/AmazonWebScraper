e437146872405275af0055a086a9aaaaf7dc7699
mariaadissa <80592742+mariaadissa@users.noreply.github.com>
2021-10-05 23:23:18 +0200

    Initial commit

---
+++ WebScraper.py
@@ -0,0 +1,56 @@
+# used libraries
+import requests
+import time
+import datetime
+import csv
+from bs4 import BeautifulSoup
+# for sending emails
+import smtplib
+
+def check_price():
+    URL = 'https://www.amazon.com/Currently-Unsupervised-Novelty-Graphic-Sarcasm/dp/B01HFFYP8A/ref=sr_1_15?dchild=1&keywords=data%2Btshirt&qid=1633430723&sr=8-15&th=1&psc=1'
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+    page = requests.get(URL, headers = headers)
+
+    soup1 = BeautifulSoup(page.content, "html.parser")
+    soup2 = BeautifulSoup(soup1.prettify(),"html.parser")
+
+    title = soup2.find(id='productTitle').get_text()
+    price = soup2.find(id="priceblock_ourprice").get_text()
+
+    price = price.strip()[1:] 
+    title = title.strip()
+    today = datetime.date.today()
+    header = ['Title', 'Price','Date']
+    data = [title, price,today]
+    with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+    
+
+# Creating the csv file for the first time
+URL = 'https://www.amazon.com/Currently-Unsupervised-Novelty-Graphic-Sarcasm/dp/B01HFFYP8A/ref=sr_1_15?dchild=1&keywords=data%2Btshirt&qid=1633430723&sr=8-15&th=1&psc=1'
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+page = requests.get(URL, headers = headers)
+soup1 = BeautifulSoup(page.content, "html.parser")
+soup2 = BeautifulSoup(soup1.prettify(),"html.parser")
+title = soup2.find(id='productTitle').get_text()
+price = soup2.find(id="priceblock_ourprice").get_text()
+price = price.strip()[1:] 
+title = title.strip()
+today = datetime.date.today()
+header = ['Title', 'Price','Date']
+data = [title, price,today]
+with open('AmazonWebScraperDataset.csv', 'w', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerow(data)  
+
+# repeating the process each day
+while(True):
+    check_price()
+    time.sleep(2) #checking the price everyday
+
+
